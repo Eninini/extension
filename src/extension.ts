cacca9f82ba295cd150b5068097adc999c7d720b
Enifer Yashmin <t-eyashmin@microsoft.com>
2024-06-12 10:33:00 +0530

    added commands to select artifacts folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,66 +1,78 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import { processJsonFile} from './processArtifacts';
+import { processJsonFile } from './processArtifacts';
 import { updateYamlDiagnostics, subscribeToDocumentChanges } from './validation';
 
+
+export var folderPath: string|undefined;
 export function activate(context: vscode.ExtensionContext) {
     //console.log('Congratulations, your extension "mobr-pipelines" is now active!');
     const yamlDiagnostics = vscode.languages.createDiagnosticCollection('yaml');
     subscribeToDocumentChanges(context, yamlDiagnostics);
 
-    let updateSettings = vscode.commands.registerCommand("mobr-pipelines.updateWorkspaceSettings", () => {
+    let updateSettings =  vscode.commands.registerCommand("mobr-pipelines.updateWorkspaceSettings", () => {
         updateWorkspaceSettings();
     });
     context.subscriptions.push(updateSettings);
-   context.subscriptions.push(
-        vscode.commands.registerCommand('mobr-pipelines.validateYaml', () => {
-            const editor = vscode.window.activeTextEditor;
-            if (editor) {
-                updateYamlDiagnostics(editor.document, yamlDiagnostics);
-            }
-               //execute commands in sequence
-        }));
-      let disposable=  vscode.commands.registerCommand('mobr-pipelines.selectServiceRootFolder', ()=>{
-            selectServiceRootFolder();
+    let disposable =  vscode.commands.registerCommand('mobr-pipelines.selectServiceRootFolder', async () => {
+       await selectServiceRootFolder();
     });
     context.subscriptions.push(disposable);
-  
+    context.subscriptions.push(
+        vscode.commands.registerCommand('mobr-pipelines.validateYaml', async () => {
+            const editor = vscode.window.activeTextEditor;
+
+            if (editor) {
+              await updateYamlDiagnostics(editor.document, yamlDiagnostics);
+            }
+            //execute commands in sequence
+        }));
+
+
 
     console.log('Congratulations, your extension "mobr-pipelines" is now active!');
 
 }
 async function selectServiceRootFolder() {
-    
-    const options: vscode.OpenDialogOptions = {
-        canSelectMany: false,
-        openLabel: 'Select Folder',
-        canSelectFolders: true,
-        canSelectFiles: false};
 
-        const folderUri= await vscode.window.showOpenDialog(options);
-        if(folderUri&&folderUri[0]){
-            const folderPath=folderUri[0].fsPath;
-            const files=fs.readdirSync(folderPath);
-           let subscriptionId: string | undefined;
-           for (const file of files){
-            const filePath =path.join(folderPath, file);
-            if(path.extname(filePath)==='.json'){
-                subscriptionId=processJsonFile(filePath);
-                if(subscriptionId){
-                    
-                }
-            }
-           }
-        }
+   try { const options: vscode.OpenDialogOptions = {
+        canSelectMany: false,
+        openLabel: 'Select Artifacts Folder',
+        canSelectFolders: true,
+        canSelectFiles: false
+    };
+
+    const folderUri = await vscode.window.showOpenDialog(options);
+    if (folderUri && folderUri[0]) {
+        folderPath = folderUri[0].fsPath;       //checking folder has some content
+
+
+        // const files = fs.readdirSync(folderPath);     //before accessing files
+        //    let subscriptionId: string | undefined;
+        //    for (const file of files){
+        //     const filePath =path.join(folderPath, file);
+        //     if(path.extname(filePath)==='.json'){
+        //         subscriptionId=processJsonFile(filePath);
+        //         if(subscriptionId){
+
+        //         }
+        //     }
+        //    }
+    }}
+    catch(error){
+        vscode.window.showErrorMessage(`failed to select service root folder: ${error}`);
+        console.log(error);
+    }
 
 }
 async function updateWorkspaceSettings() {
     const config = vscode.workspace.getConfiguration("yaml");
     let settings = config.get<Record<string, any>>("schemas") || {};
-
+console.log(settings);
     // Define the path to your JSON schema file
     const schemaFileName = "mobrSchema.json";
+    console.log(__dirname);
     const schemaFilePath = path.join(__dirname, '..', 'schemas', schemaFileName);
     console.log('Schema file path:', schemaFilePath);
     const schemaFileUri = vscode.Uri.file(schemaFilePath).toString();
@@ -72,8 +84,8 @@ async function updateWorkspaceSettings() {
         return;
     }
 
-   //const yamlFilePath = activeEditor.document.uri.fsPath;
-    const yamlFilePath="/*";
+    //const yamlFilePath = activeEditor.document.uri.fsPath;
+    const yamlFilePath = "/*";
 
     if (!settings[schemaFilePath]) {
         settings[schemaFilePath] = [];
@@ -86,15 +98,15 @@ async function updateWorkspaceSettings() {
     console.log('Updating YAML schemas settings...');
     try {
         await config.update("schemas", settings, vscode.ConfigurationTarget.Workspace, true);
-        
+
         vscode.window.showInformationMessage(`Updated YAML schema settings for ${yamlFilePath}`);
         console.log('Updated YAML schema settings:', settings);
-       
+
     } catch (error) {
         vscode.window.showErrorMessage(`Failed to update YAML schema settings: ${error}`);
         console.log('Failed to update YAML schema settings:', error);
     }
-    
+
 }
 
 export function deactivate() {
